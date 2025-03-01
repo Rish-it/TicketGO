4cf1f2c4b2897743df761035adc40485f7a3aab0
Rish-it <201b214@juetguna.in>
2025-03-01 13:18:35 +0530

    Initialize Go project with module support

--- main.go
+++ main.go
@@ -8,6 +8,6 @@ func main() {
 	var availableTickets = 100
 	fmt.Print("Last Episode of the century", showName)
 	fmt.Print("Tickets are available", showTickets)
-	fmt.Print("Booking Your Seat Now", availableTickets, "Only few are left")
+	fmt.Print("Booking Your Seat Now", availableTickets, "Only few are left") // to refactor better you can just have a placeholder %v  whereever you want to use your variables
 
 }
