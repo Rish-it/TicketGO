51150488cccff83bb55757069a942b371dc3422d
Rish-it <201b214@juetguna.in>
2025-03-01 15:08:17 +0530

    handling the booking list gracefully

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	// var showName = "India's Got Latent"
@@ -13,29 +16,38 @@ func main() {
 	fmt.Println("Tickets are available", showTickets)
 	fmt.Println("Booking Your Seat Now", availableTickets, "Only few are left") // to refactor better you can just have a placeholder %v  whereever you want to use your variables
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	fmt.Println("Enter Your First Name:")
-	fmt.Scan(&firstName)
+		fmt.Println("Enter Your First Name:")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Enter Your Last Name:")
-	fmt.Scan(&lastName)
+		fmt.Println("Enter Your Last Name:")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Enter Your Email:")
-	fmt.Scan(&email)
+		fmt.Println("Enter Your Email:")
+		fmt.Scan(&email)
 
-	fmt.Println("Enter Number of Tickets:")
-	fmt.Scan(&userTickets) //------> in order to make this work we need a pointer to the address.
+		fmt.Println("Enter Number of Tickets:")
+		fmt.Scan(&userTickets) //------> in order to make this work we need a pointer to the address.
 
-	availableTickets = availableTickets - userTickets
-	bookings = append(bookings, firstName+" "+lastName) //--- using slice for dynamic list mng
+		availableTickets = availableTickets - userTickets
+		bookings = append(bookings, firstName+" "+lastName) //--- using slice for dynamic list mng
 
-	fmt.Printf("Thankyou %v for choosing us and booking %v tickets. You will receive a confirmation email at %v\n", firstName, userTickets, email)
+		fmt.Printf("Thankyou %v for choosing us and booking %v tickets. You will receive a confirmation email at %v\n", firstName, userTickets, email)
 
-	fmt.Printf("Book fast only few are left %v\n", availableTickets)
+		fmt.Printf("Book fast only few are left %v\n", availableTickets)
 
-	fmt.Printf("List of Attendees %v", bookings)
+		firstNames := []string{}
+		for _, booking := range bookings { //------>used variable
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+		fmt.Printf("List of Attendees %v", firstNames)
+
+	}
+
 }
