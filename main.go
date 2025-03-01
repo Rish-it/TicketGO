19d4decffe4f4776ddd7db9d62f3cf463f6175fb
Rish-it <201b214@juetguna.in>
2025-03-01 13:34:18 +0530

    refactoring is optional at this time

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	var showName = "India's Got Latent"
+	// var showName = "India's Got Latent"
+	showName := "India's Got Latent" //syntax sugar but only with variables not possible with "const"
 	const showTickets = 100
 	var availableTickets = 100
+
 	fmt.Print("Last Episode of the century", showName)
 	fmt.Print("Tickets are available", showTickets)
 	fmt.Print("Booking Your Seat Now", availableTickets, "Only few are left") // to refactor better you can just have a placeholder %v  whereever you want to use your variables
 
+	var userName string
+
+	fmt.Scan(userName) //------> in order to make this work we need a pointer to the address.
 }
